7b812851779e5324085393ab1606558624452609
EladTusr <90709304+EladTusr@users.noreply.github.com>
2021-09-19 01:43:48 +0300

    Add files via upload

---
+++ StackoverfowData/StackoverflowData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPAnalyzer1
+{
+    /*
+     * the lower data model of the Stackoverflow API
+     */
+    public class StackoverflowData
+    {
+        public string title { get; set; }
+    }
+}
---
+++ StackoverfowData/StackoverflowTopModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPAnalyzer1
+{
+    /*
+     * the upper data model of the Stackoverflow API
+     */
+    public class StackoverflowTopModel
+    {
+        public StackoverflowData[] Items { get; set; }
+    }
+}
