32e01890df92524449fd4ab61b5df1aae692c5f1
EladTusr <90709304+EladTusr@users.noreply.github.com>
2021-09-19 01:42:30 +0300

    Add files via upload

---
+++ DataProcessors/GITProcessor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net;
+
+
+namespace OPAnalyzer1
+{
+     /*
+      * This class will read the data from the github Api.
+      * the read will be in string format which means the whole site's data as one string.
+      * after the data has been obtaned, It will be sent to several methods so it will be returned in the reqested format
+      */
+    public class GITProcessor
+    {
+        public static async Task<List<string>>  LoadGitData(HttpClient APIClient)
+        {
+            using (HttpResponseMessage response = await APIClient.GetAsync(""))
+            {
+                if (response.IsSuccessStatusCode)
+                {   
+                    string my_str = await response.Content.ReadAsStringAsync();
+                    string[] my_array = my_str.Split("message\":");
+                    my_array = MyArrayHelper.removeTheRest("\",", my_array);
+                    my_array = MyArrayHelper.removeFirstCell(my_array);
+                    my_array = MyArrayHelper.removeApostrophes(my_array);
+                    List<string> outputList = MyArrayHelper.moveToList(my_array);
+                    return outputList;
+
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
---
+++ DataProcessors/StackoverflowProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace OPAnalyzer1
+{
+    /*
+     * In this class we will call a method which will read and data from the Stackoverflow Api 
+     * the relevant data is represented as StackoverflowTopModel which is a calss that contains onley the relevant path to the title property
+     * with the use of the function ReadAsAsync inly the relevant data will be obtained from the API, while tho irelevant data will be ignored.
+     * this class will return the data in the orgenized metheod as StackoverflowTopModel.
+     */
+    public class StackoverflowProcessor
+    {
+        public static async Task<StackoverflowTopModel> LoadStackoverflowData(HttpClient  APIClient)
+        {
+            using (HttpResponseMessage response = await APIClient.GetAsync("faq?site=stackoverflow"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    StackoverflowTopModel data_model = await response.Content.ReadAsAsync<StackoverflowTopModel>();
+                    return data_model;
+
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
